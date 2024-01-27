ca0201a6e87cf9f94eaff47cb02f9685b6076697
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-27 10:36:46 +0600

    added middleware to reduxtoolkit

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -1,11 +1,17 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
-
+const reduxLogger = require('redux-logger')
+const icecreamReducer = require('../features/icecream/icecreamSlice')
 const cakeReducer = require('../features/cake/cakeSlice')
 
+
+const logger = reduxLogger.createLogger()
 const store =  configureStore({
     reducer:{
         cakes: cakeReducer,
+        iceCreams: icecreamReducer,
+
     },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,15 +1,19 @@
-const store = require('./app/store')
+const store = require('./app/store');
+const iceCreamActions = require('./features/icecream/icecreamSlice').iceCreamActions;
+const cakeAction = require('./features/cake/cakeSlice.js').cakeAction;
 
-const cakeAction = require('./features/cake/cakeSlice.js').cakeAction
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state', store.getState());
+});
 
-console.log('Initial state', store.getState())
-const unsubscribe = store.subscribe(()=>{
-    console.log('updated state', store.getState());
-})
+store.dispatch(cakeAction.ordered());
+store.dispatch(cakeAction.ordered());
+store.dispatch(cakeAction.ordered());
+store.dispatch(cakeAction.restocked(3));
 
-store.dispatch(cakeAction.ordered())
-store.dispatch(cakeAction.ordered())
-store.dispatch(cakeAction.ordered())
-store.dispatch(cakeAction.restocked(3))
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(2));
 
-unsubscribe()
+unsubscribe();
